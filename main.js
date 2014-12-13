01c70b74b031b196a28c03b6bb19607fdea3de85
elitepc <pedrocaseiro7@gmail.com>
2014-12-13 19:31:56 +0000

    started start function

---
+++ main.js
@@ -0,0 +1,20 @@
+(function () {
+	'use strict';
+
+	//initialize globals
+	var url = "";
+
+	//start script
+	function startScript(){
+		url = window.location.href; //get url
+		
+		$('#ds_body').empty();
+		$('#ds_body').removeAttr('id');
+		
+		
+	}
+	
+	
+	
+	
+}());
